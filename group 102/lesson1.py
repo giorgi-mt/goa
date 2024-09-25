fe7404920ab1a9f98021e2395d4d51028365c131
giorgi-mt <ggiioomentesh@gmail.com>
2024-09-25 22:07:37 +0400

    first commit

---
+++ group 102/lesson1.py
@@ -0,0 +1,27 @@
+name="gio"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "gio"  არის ცვლადისთვის მნიშვნელობა
+
+surname="menteshashvili"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე ობიექტი
+
+name="gio" # ეს არის str (string) ტიპის ცვლადი
+age=9 #ეს არის  int ( integer ) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (booL) ტიპის ცვლადი
+
+know_programing = True #Teue ან  False
+is_ugly = False  # snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+isugly = False # ჯავასკრიფტილი camelcase
+
+
+#print(name+ " " +surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name +  age)
+
+print(type(age))
