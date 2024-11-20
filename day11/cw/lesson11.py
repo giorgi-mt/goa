2bc0147c23643ad58fa6bfe4fc75490733664fdd
giorgi-mt <ggiioomentesh@gmail.com>
2024-11-20 21:20:27 +0400

    day11

---
+++ day11/cw/lesson11.py
@@ -0,0 +1,8 @@
+num1=10<9
+num2=9>8
+num3=3>1
+num4=3>2
+print(num1 and num2)
+print(num3 and num4)
+
+print("messi is the goat" and "ronaldo is the bed")
---
+++ day11/hw/lesson11.py
@@ -0,0 +1,15 @@
+print(40<59)
+num1=48<32
+num2=56<65
+print(num1 and num2)
+num3=78<79
+num4=76>76
+print(num3 or num4)
+
+num5=10<15
+num6=20<26
+print(num5)
+print(num6)
+num7=54
+num8=43
+print(num7>num8)
