be0afdd7f5f6722de9f73cb2deaf6e785c8fab8f
giorgi-mt <ggiioomentesh@gmail.com>
2025-03-06 22:45:13 +0400

    commit

---
+++ day26/cw/26cw.py
@@ -0,0 +1,14 @@
+age=[1,2,3,4,5,6,7,8,9,10]
+age[1]="giorgi"
+
+age[2]="taia"
+
+age[3]="menteshashvili"
+
+age[4]="tamta"
+
+age[5]="andria"
+
+print(age[0:9])
+print(age[-1:-5])
+
---
+++ day26/hw/26hw.py
@@ -0,0 +1,17 @@
+age=[1,2,3,4,5,6,7,8,9,10]
+print(age[5:8])
+#slicing გვეხმარება რათა რაღაც ინდექსიდან რარაც ინდექსამდე  მივიყვანოთ რამე მაგ  5 ინდექსიდან  10 ინდექსამდე.
+num1=[10,9,8,7,6,5,4,3,2,]
+print(num1[-9:-0])
+novator=[novator,world,the,best,,goa,is,the,bad,novatorbest]
+novator[novator]=goa
+novator[world]=world
+novator[the]=the
+novator[best]=best
+novator[goa]=novator
+novator[is]=is
+novator[the]=the
+novator[bad]=BAD
+novator[novatorbest]=goabest
+print(novator)
+#list არის ოთხკუთხედებში მოქცეული სიტყვები.
