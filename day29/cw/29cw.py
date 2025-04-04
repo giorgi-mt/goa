d6ac5c3af7ff68e46dece00e8f509ddf1d4464f9
giorgi-mt <ggiioomentesh@gmail.com>
2025-04-04 10:58:25 +0400

    commit

---
+++ day29/cw/29cw.py
@@ -0,0 +1,6 @@
+def saxeli(taia_menteshashvili):
+      
+   print(taia_menteshashvili)
+
+
+saxeli("giorgi_menteshashvili")
---
+++ day29/hw/29hw.py
@@ -0,0 +1,18 @@
+def addnumbers(x,b):
+    print(x,b)
+
+addnumbers("XB")
+
+
+
+def hello(name):
+    print(name)
+
+    hello("giorgi_menteshashvili")
+
+
+def Multiply_Number(x,b):
+    print(x,b)
+
+    Multiply_Number("x*b")
+
