4de32cc4d929efde29ae82b00773bc97d0a532e1
giorgi-mt <ggiioomentesh@gmail.com>
2025-08-26 09:41:38 +0400

    commit

---
+++ day49/cw/cw49.py
@@ -0,0 +1,22 @@
+
+def func(goa,goa1):
+
+    print(goa * goa1)
+   
+func("giorgi",4)
+
+def func1(boleom,goa2):
+    if boleom == true
+    
+    print(boleom+goa2)
+
+func1(true,"goabest")
+
+def func2(text,int):
+     print(text,int)
+
+func2([123456]7)
+
+def func3(text):
+    print(text)
+func3(5)
